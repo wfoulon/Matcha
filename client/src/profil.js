9655db287dfa86cbd0f61e5ebe0f597324fb6159
Florian GAZEAU <flgazeau@e2r3p2.42.fr>
2018-07-19 11:36:02 +0200

    ok

--- client/src/profil.js
+++ client/src/profil.js
@@ -57,23 +57,23 @@ class Profil extends React.Component {
                             <div>Sexual orentation: </div> */}
 
                             {/* <!-- Card --> */}
-                            <div class="card mb-4">
+                            <div className="card mb-4">
 
                                 {/* <!-- Card image --> */}
-                                <div class="view overlay">
-                                <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/49.jpg" alt="Card image cap"/>
+                                <div className="view overlay">
+                                <img className="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/49.jpg" alt="Card image cap"/>
                                 <a href="#!">
-                                    <div class="mask rgba-white-slight"></div>
+                                    <div className="mask rgba-white-slight"></div>
                                 </a>
                                 </div>
 
                                 {/* <!-- Card content --> */}
-                                <div class="card-body">
+                                <div className="card-body">
 
                                 {/* <!-- Title --> */}
-                                <h4 class="card-title">{localStorage.fname} {localStorage.lname} {localStorage.login}</h4>
+                                <h4 className="card-title">{localStorage.fname} {localStorage.lname} {localStorage.login}</h4>
                                 {/* <!-- Text --> */}
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                 {/* <!-- Provides extra visual weight and identifies the primary action in a set of buttons --> */}
                                 {/* <button type="button" class="btn btn-primary btn-md">Read more</button> */}
 
